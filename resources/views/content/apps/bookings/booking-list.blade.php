ed397324bdf9297e0897fd8fbd232ea237d6d22c
valdimiro francisco <valdimiroeinstein@gmail.com>
2022-03-21 15:35:54 +0200

    modal changed

--- resources/views/content/apps/bookings/booking-list.blade.php
+++ resources/views/content/apps/bookings/booking-list.blade.php
@@ -11,6 +11,7 @@
   <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/rowGroup.bootstrap5.min.css')) }}">
   <link rel="stylesheet" href="{{ asset(mix('vendors/css/pickers/flatpickr/flatpickr.min.css')) }}">
   <link rel="stylesheet" href="{{ asset(mix('vendors/css/pickers/flatpickr/flatpickr.min.css')) }}">
+  <link rel="stylesheet" href="{{ asset(mix('vendors/css/forms/wizard/bs-stepper.min.css')) }}">
 
 @endsection
 
@@ -18,6 +19,7 @@
   {{-- Page Css files --}}
   <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-validation.css')) }}">
   <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/pickers/form-flat-pickr.css')) }}">
+  <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-wizard.css')) }}">
 @endsection
 
 @section('content')
@@ -111,7 +113,7 @@
       </table>
     </div>
     <!-- Modal to add new user starts-->
-    <div class="modal modal-slide-in new-user-modal fade" id="modals-slide-in">
+    {{-- <div class="modal modal-slide-in new-user-modal fade" id="modals-slide-in">
       <div class="modal-dialog">
         <form class="add-new-user modal-content pt-0">
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">×</button>
@@ -245,8 +247,299 @@
           </div>
         </form>
       </div>
-    </div>
+    </div> --}}
     <!-- Modal to add new user Ends-->
+
+    <!-- Modal -->
+    <div class="modal fade text-start" id="modals-slide-in" tabindex="-1" aria-labelledby="myModalLabel17" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title" id="myModalLabel17">Nova Reserva</h4>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="bs-stepper vertical wizard-modern modern-vertical-wizard-example">
+            <div class="bs-stepper-header">
+              <div
+                class="step"
+                data-target="#account-details-vertical-modern"
+                role="tab"
+                id="account-details-vertical-modern-trigger"
+              >
+                <button type="button" class="step-trigger">
+                  <span class="bs-stepper-box">
+                    <i data-feather="user" class="font-medium-3"></i>
+                  </span>
+                  <span class="bs-stepper-label">
+                    <span class="bs-stepper-title">Cliente</span>
+                    <span class="bs-stepper-subtitle">Detalhes do cliente</span>
+                  </span>
+                </button>
+              </div>
+              <div
+                class="step"
+                data-target="#personal-info-vertical-modern"
+                role="tab"
+                id="personal-info-vertical-modern-trigger"
+              >
+                <button type="button" class="step-trigger">
+                  <span class="bs-stepper-box">
+                    <i data-feather="truck" class="font-medium-3"></i>
+                  </span>
+                  <span class="bs-stepper-label">
+                    <span class="bs-stepper-title">Viatura</span>
+                    <span class="bs-stepper-subtitle">Detalhes da viatura</span>
+                  </span>
+                </button>
+              </div>
+              <div
+                class="step"
+                data-target="#address-step-vertical-modern"
+                role="tab"
+                id="address-step-vertical-modern-trigger"
+              >
+                <button type="button" class="step-trigger">
+                  <span class="bs-stepper-box">
+                    <i data-feather="map-pin" class="font-medium-3"></i>
+                  </span>
+                  <span class="bs-stepper-label">
+                    <span class="bs-stepper-title">Destino</span>
+                    <span class="bs-stepper-subtitle">Detalhes do destino</span>
+                  </span>
+                </button>
+              </div>
+              <div
+                class="step"
+                data-target="#social-links-vertical-modern"
+                role="tab"
+                id="social-links-vertical-modern-trigger"
+              >
+                <button type="button" class="step-trigger">
+                  <span class="bs-stepper-box">
+                    <i data-feather="link" class="font-medium-3"></i>
+                  </span>
+                  <span class="bs-stepper-label">
+                    <span class="bs-stepper-title">Outros</span>
+                  </span>
+                </button>
+              </div>
+            </div>
+            <div class="bs-stepper-content">
+              <div
+                id="account-details-vertical-modern"
+                class="content"
+                role="tabpanel"
+                aria-labelledby="account-details-vertical-modern-trigger"
+              >
+                <div class="content-header">
+                  <h5 class="mb-0">Cliente</h5>
+                  <small class="text-muted">Preencha os detalhes do cliente.</small>
+                </div>
+                <div class="row">
+                  <div class="mb-1 col-md-6">
+                    <label class="form-label" for="vertical-modern-username">Nome</label>
+                    <input type="text" id="vertical-modern-username" class="form-control" placeholder="johndoe" />
+                  </div>
+                  <div class="mb-1 col-md-6">
+                    <label class="form-label" for="vertical-modern-email">E-Mail</label>
+                    <input
+                      type="email"
+                      id="vertical-modern-email"
+                      class="form-control"
+                      placeholder="john.doe@email.com"
+                      aria-label="john.doe"
+                    />
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="mb-1 col-md-6">
+                    <label class="form-label" for="vertical-modern-username">Contacto</label>
+                    <input type="text" id="vertical-modern-username" class="form-control" placeholder="johndoe" />
+                  </div>
+                  <div class="mb-1 col-md-6">
+                    <label class="form-label" for="vertical-modern-username">Endereço</label>
+                    <input type="text" id="vertical-modern-username" class="form-control" placeholder="johndoe" />
+                  </div>
+                  
+                </div>
+                <div class="d-flex justify-content-between">
+                  <button class="btn btn-outline-secondary btn-prev" disabled>
+                    <i data-feather="arrow-left" class="align-middle me-sm-25 me-0"></i>
+                    <span class="align-middle d-sm-inline-block d-none">Anterior</span>
+                  </button>
+                  <button class="btn btn-primary btn-next">
+                    <span class="align-middle d-sm-inline-block d-none">Proximo</span>
+                    <i data-feather="arrow-right" class="align-middle ms-sm-25 ms-0"></i>
+                  </button>
+                </div>
+              </div>
+              <div
+                id="personal-info-vertical-modern"
+                class="content"
+                role="tabpanel"
+                aria-labelledby="personal-info-vertical-modern-trigger"
+              >
+                <div class="content-header">
+                  <h5 class="mb-0">Personal Info</h5>
+                  <small>Enter Your Personal Info.</small>
+                </div>
+                <div class="row">
+                  <div class="mb-1 col-md-6">
+                    <label class="form-label" for="vertical-modern-first-name">First Name</label>
+                    <input type="text" id="vertical-modern-first-name" class="form-control" placeholder="John" />
+                  </div>
+                  <div class="mb-1 col-md-6">
+                    <label class="form-label" for="vertical-modern-last-name">Last Name</label>
+                    <input type="text" id="vertical-modern-last-name" class="form-control" placeholder="Doe" />
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="mb-1 col-md-6">
+                    <label class="form-label" for="vertical-modern-country">Country</label>
+                    <select class="select2 w-100" id="vertical-modern-country">
+                      <option label=" "></option>
+                      <option>UK</option>
+                      <option>USA</option>
+                      <option>Spain</option>
+                      <option>France</option>
+                      <option>Italy</option>
+                      <option>Australia</option>
+                    </select>
+                  </div>
+                  <div class="mb-1 col-md-6">
+                    <label class="form-label" for="vertical-modern-language">Language</label>
+                    <select class="select2 w-100" id="vertical-modern-language" multiple>
+                      <option>English</option>
+                      <option>French</option>
+                      <option>Spanish</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="d-flex justify-content-between">
+                  <button class="btn btn-primary btn-prev">
+                    <i data-feather="arrow-left" class="align-middle me-sm-25 me-0"></i>
+                    <span class="align-middle d-sm-inline-block d-none">Previous</span>
+                  </button>
+                  <button class="btn btn-primary btn-next">
+                    <span class="align-middle d-sm-inline-block d-none">Next</span>
+                    <i data-feather="arrow-right" class="align-middle ms-sm-25 ms-0"></i>
+                  </button>
+                </div>
+              </div>
+              <div
+                id="address-step-vertical-modern"
+                class="content"
+                role="tabpanel"
+                aria-labelledby="address-step-vertical-modern-trigger"
+              >
+                <div class="content-header">
+                  <h5 class="mb-0">Address</h5>
+                  <small>Enter Your Address.</small>
+                </div>
+                <div class="row">
+                  <div class="mb-1 col-md-6">
+                    <label class="form-label" for="vertical-modern-address">Address</label>
+                    <input
+                      type="text"
+                      id="vertical-modern-address"
+                      class="form-control"
+                      placeholder="98  Borough bridge Road, Birmingham"
+                    />
+                  </div>
+                  <div class="mb-1 col-md-6">
+                    <label class="form-label" for="vertical-modern-landmark">Landmark</label>
+                    <input type="text" id="vertical-modern-landmark" class="form-control" placeholder="Borough bridge" />
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="mb-1 col-md-6">
+                    <label class="form-label" for="pincode4">Pincode</label>
+                    <input type="text" id="pincode4" class="form-control" placeholder="658921" />
+                  </div>
+                  <div class="mb-1 col-md-6">
+                    <label class="form-label" for="city4">City</label>
+                    <input type="text" id="city4" class="form-control" placeholder="Birmingham" />
+                  </div>
+                </div>
+                <div class="d-flex justify-content-between">
+                  <button class="btn btn-primary btn-prev">
+                    <i data-feather="arrow-left" class="align-middle me-sm-25 me-0"></i>
+                    <span class="align-middle d-sm-inline-block d-none">Previous</span>
+                  </button>
+                  <button class="btn btn-primary btn-next">
+                    <span class="align-middle d-sm-inline-block d-none">Next</span>
+                    <i data-feather="arrow-right" class="align-middle ms-sm-25 ms-0"></i>
+                  </button>
+                </div>
+              </div>
+              <div
+                id="social-links-vertical-modern"
+                class="content"
+                role="tabpanel"
+                aria-labelledby="social-links-vertical-modern-trigger"
+              >
+                <div class="content-header">
+                  <h5 class="mb-0">Social Links</h5>
+                  <small>Enter Your Social Links.</small>
+                </div>
+                <div class="row">
+                  <div class="mb-1 col-md-6">
+                    <label class="form-label" for="vertical-modern-twitter">Twitter</label>
+                    <input
+                      type="text"
+                      id="vertical-modern-twitter"
+                      class="form-control"
+                      placeholder="https://twitter.com/abc"
+                    />
+                  </div>
+                  <div class="mb-1 col-md-6">
+                    <label class="form-label" for="vertical-modern-facebook">Facebook</label>
+                    <input
+                      type="text"
+                      id="vertical-modern-facebook"
+                      class="form-control"
+                      placeholder="https://facebook.com/abc"
+                    />
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="mb-1 col-md-6">
+                    <label class="form-label" for="vertical-modern-google">Google+</label>
+                    <input
+                      type="text"
+                      id="vertical-modern-google"
+                      class="form-control"
+                      placeholder="https://plus.google.com/abc"
+                    />
+                  </div>
+                  <div class="mb-1 col-md-6">
+                    <label class="form-label" for="vertical-modern-linkedin">Linkedin</label>
+                    <input
+                      type="text"
+                      id="vertical-modern-linkedin"
+                      class="form-control"
+                      placeholder="https://linkedin.com/abc"
+                    />
+                  </div>
+                </div>
+                <div class="d-flex justify-content-between">
+                  <button class="btn btn-primary btn-prev">
+                    <i data-feather="arrow-left" class="align-middle me-sm-25 me-0"></i>
+                    <span class="align-middle d-sm-inline-block d-none">Previous</span>
+                  </button>
+                  <button data-bs-dismiss="modal" type="button" class="btn btn-success btn-submit">Submit</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+   </div>
+
+
   </div>
   <!-- list and filter end -->
 </section>
@@ -275,12 +568,14 @@
   <script src="{{ asset(mix('vendors/js/pickers/pickadate/picker.time.js')) }}"></script>
   <script src="{{ asset(mix('vendors/js/pickers/pickadate/legacy.js')) }}"></script>
   <script src="{{ asset(mix('vendors/js/pickers/flatpickr/flatpickr.min.js')) }}"></script>
+  <script src="{{ asset(mix('vendors/js/forms/wizard/bs-stepper.min.js')) }}"></script>
 
 @endsection
 
 @section('page-script')
   {{-- Page js files --}}
   <script src="{{ asset(mix('js/scripts/forms/pickers/form-pickers.js')) }}"></script>
+  <script src="{{ asset(mix('js/scripts/forms/form-wizard.js')) }}"></script>
   <script src="{{ asset(mix('js/scripts/pages/app-booking-list.js')) }}"></script>
 @endsection
 
